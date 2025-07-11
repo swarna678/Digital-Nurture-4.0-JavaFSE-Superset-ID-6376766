29d148b47b7e8872e7caa8e0a842ec93f639993f
Swarna Latha <swarnalatha162403@gmail.com>
2025-07-11 14:52:27 +0530

    Updated Week 3 tasks

--- Week - 3/Spring_Core_Maven/Exercise 1 -Configuring a Basic Spring Application/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
+++ Week - 3/Spring_Core_Maven/Exercise 1 -Configuring a Basic Spring Application/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -1,7 +1,9 @@
 package com.library.repository;
 
 public class BookRepository {
+    // it is repository
     public void showRepo() {
         System.out.println("BookRepository: Accessing books from DB.");
+        
     }
 }
